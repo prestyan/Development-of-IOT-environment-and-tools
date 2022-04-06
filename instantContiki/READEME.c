8335e6bcbbc0ad6a8bdef72e6dcdc5ac3f91cc6b
prestyan <1517341399@qq.com>
2022-04-06 19:42:25 +0800

    Read me first.

---
+++ instantContiki/READEME.c
@@ -0,0 +1,15 @@
+//This pdf tells you how to use InstanContiki.
+//And how codes run while simulating WSNs transmitting information.
+//I've design some experiments to test and compare several protocols applied to diffreent sights and application environment.
+//All you can see in the file.
+//Q:You must care if your computer are outdated for running contiki slowly.
+//A:No, you will find answer in pdf!
+#include<stdio.h>
+int main()
+{
+ puts("Author:ShaoYang(prestyan)");
+ puts("e-mail:shaoyang@nuaa.edu.cn");
+ puts("CopyRight_All rights reserved.");
+ puts("2022.4.6 in nuaa NanJing City, JiangSu Pro, China");
+ return 0;
+}
